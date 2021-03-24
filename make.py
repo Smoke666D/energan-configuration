c463aad0ed6652154a3fe61e357fb9497024abef
Mike <photo.mickey@gmail.com>
2021-03-24 12:34:53 +0300

    Output data

--- make.py
+++ make.py
@@ -198,7 +198,7 @@ def readCSV ( fileName ):
                     bitCnt = bitCnt + bl;
                     map[-1].bit.append( bm.__dict__ );
     print( 'Done!' );
-    return [map, maxUnitsLen, maxRegNumber];
+    return [map, maxUnitsLen, maxRegNumber, regNumber];
 #*******************************************************************************
 def makeJSON ( map, fileName, structName ):
     print( '>>Make JSON...' );
@@ -269,6 +269,98 @@ class metaDataC ():
     maxScale          = 0;
     bitAdresses       = "";
     totalSize         = 0;
+    maxValueLen       = 0;
+    postArray         = "";
+    dictionaryArray   = "";
+#*******************************************************************************
+def recordToStruct ( row, file, meta ):
+    if ( row.bitMapSize > 0 ):
+        bitAdr = 0;
+        file.write( "const eConfigBitMap " + row.name + "BitMap[" + str(row.bitMapSize) + "U] = \n" );
+        file.write( "{\n" );
+        first = 0;
+        for bm in row.bit:
+            meta.totalSize += 5;
+            first     += 1;
+            file.write( "   { " );
+            file.write( str( bm["mask"]  ) + "U, " );
+            file.write( str( bm["shift"] ) + "U "  );
+            file.write( "},     // " + bm["name"] + "\n" );
+            defStr = "";
+            defStr += "#define   " + makeDefineStr( bm["name"], "ADR" );
+            for i in range( len( defStr ), 55, 1 ):
+                defStr += " ";
+            defStr      += str( bitAdr ) + 'U';
+            defStr      += "\r\n";
+            meta.bitAdresses += defStr;
+            bitAdr      += 1;
+        file.write( '};\n' );
+    if ( row.rw == "r" ):
+        file.write( "const " )
+    file.write( "uint16_t " + row.name + "Value[" + str(row.len) + "U] = { " );
+    if ( row.len > meta.maxValueLen ):
+        meta.maxValueLen = row.len;
+    meta.totalSize += 17;
+    for i in range( 0, row.len ):
+        meta.totalSize += 2;
+        if row.type == 'C':
+            file.write("0x0020U");
+        else:
+            file.write( str( ( row.value >> 16 * i ) & 0xFFFF ) + "U" );
+        if i < ( row.len - 1 ):
+            file.write( ", " );
+    file.write( " };\n" );
+    file.write( "const eConfigAttributes " + row.name + "Atrib =\n{\n" );
+    file.write( "   .adr        = " + str( row.adr )   + "U,\n" );
+    if ( row.scale < meta.minScale ):
+        meta.minScale = row.scale;
+    if ( row.scale > meta.maxScale ):
+        meta.maxScale = row.scale;
+    if ( row.scale >= 0 ):
+        file.write( "   .scale      = " + str( int( row.scale ) ) + "U,\n" );
+    else:
+        file.write( "   .scale      = " + str(row.scale) + ",\n" );
+    file.write( "   .min        = " + str( row.min )   + "U,\n" );
+    if ( row.max > 65535 ):
+        row.max = 65535;
+    file.write( "   .max        = " + str( row.max )   + "U,\n" );
+    file.write( "   .units      = { " );
+    i = 0;
+    l = list( row.units );
+    while i < maxUnitsLen:
+        if len( row.units ) > i:
+            file.write( cyrillic2hex( l[i] ) );
+        else:
+            file.write( "0x0020U" );
+        if i != ( maxUnitsLen - 1 ):
+            file.write( ", " );
+        i = i + 1;
+    file.write( " },\n" );
+    if ( row.bitMapSize > 0 ):
+        file.write( "   .type       = " + setClass( 'B' ) + ",\n" );
+    else:
+        file.write( "   .type       = " + setClass( row.type ) + ",\n" );
+    file.write( "   .len        = " + str( row.len )   + "U,\n" );
+    file.write( "   .bitMapSize = " + str( row.bitMapSize ) + "U,\n" );
+    if ( row.bitMapSize > 0 ):
+        file.write( "   .bitMap     = " + row.name + "BitMap\n" );
+    else:
+        file.write( "   .bitMap     = NULL,\n" );
+    if ( row.bitMapSize > meta.maxBitMapSize ):
+        meta.maxBitMapSize = row.bitMapSize;
+    meta.configStorageSize += ( row.len * 2 ) + 1;
+    file.write( '};\n' );
+    if ( row.rw == "r" ):
+        file.write( "const " );
+    file.write( "eConfigReg " + row.name + " =\n" );
+    meta.postArray       += "&" + row.name + ", ";
+    meta.dictionaryArray += '"' + row.str + '", ';
+    file.write( '{\n');
+    file.write( "   .atrib = &" + row.name + "Atrib,\n" );
+    file.write( "   .value = " + row.name + "Value,\n" );
+    file.write( "};\n" );
+    file.write( "/*----------------------------------------------------------------*/\n" );
+    return;
 
 def makeC_config ( map, fileName, structName ):
     print( ">>Make C configuration...");
@@ -280,103 +372,18 @@ def makeC_config ( map, fileName, structName ):
     f.write( " */\n" );
     f.write( "#include   \"config.h\"\n" );
     f.write( "\n" );
-    postArray       = "eConfigReg* const " + structName + "Reg[SETTING_REGISTER_NUMBER]  = { ";
-    dictionaryArray = "const char*       " + structName + "Dictionary[SETTING_REGISTER_NUMBER] = { ";
-    maxValueLen     = 0;
+    meta.postArray        = "eConfigReg* const " + structName + "Reg[SETTING_REGISTER_NUMBER]  = { ";
+    meta.dictionaryArray  = "const char*       " + structName + "Dictionary[SETTING_REGISTER_NUMBER] = { ";
+    meta.maxValueLen = 0;
     for row in map:
-        if ( row.bitMapSize > 0 ):
-            bitAdr = 0;
-            f.write( "const eConfigBitMap " + row.name + "BitMap[" + str(row.bitMapSize) + "U] = \n" );
-            f.write( "{\n" );
-            first = 0;
-            for bm in row.bit:
-                meta.totalSize += 5;
-                first     += 1;
-                f.write( "   { " );
-                f.write( str( bm["mask"]  ) + "U, " );
-                f.write( str( bm["shift"] ) + "U "  );
-                f.write( "},     // " + bm["name"] + "\n" );
-                defStr = "";
-                defStr += "#define   " + makeDefineStr( bm["name"], "ADR" );
-                for i in range( len( defStr ), 55, 1 ):
-                    defStr += " ";
-                defStr      += str( bitAdr ) + 'U';
-                defStr      += "\r\n";
-                meta.bitAdresses += defStr;
-                bitAdr      += 1;
-            f.write( '};\n' );
-        if ( row.rw == "r" ):
-            f.write( "const " )
-        f.write( "uint16_t " + row.name + "Value[" + str(row.len) + "U] = { " );
-        if ( row.len > maxValueLen ):
-            maxValueLen = row.len;
-        meta.totalSize += 17;
-        for i in range( 0, row.len ):
-            meta.totalSize += 2;
-            if row.type == 'C':
-                f.write("0x0020U");
-            else:
-                f.write( str( ( row.value >> 16 * i ) & 0xFFFF ) + "U" );
-            if i < ( row.len - 1 ):
-                f.write( ", " );
-        f.write( " };\n" );
-        f.write( "const eConfigAttributes " + row.name + "Atrib =\n{\n" );
-        f.write( "   .adr        = " + str( row.adr )   + "U,\n" );
-        if ( row.scale < meta.minScale ):
-            meta.minScale = row.scale;
-        if ( row.scale > meta.maxScale ):
-            meta.maxScale = row.scale;
-        if ( row.scale >= 0 ):
-            f.write( "   .scale      = " + str( int( row.scale ) ) + "U,\n" );
-        else:
-            f.write( "   .scale      = " + str(row.scale) + ",\n" );
-        f.write( "   .min        = " + str( row.min )   + "U,\n" );
-        if ( row.max > 65535 ):
-            row.max = 65535;
-        f.write( "   .max        = " + str( row.max )   + "U,\n" );
-        f.write( "   .units      = { " );
-        i = 0;
-        l = list( row.units );
-        while i < maxUnitsLen:
-            if len( row.units ) > i:
-                f.write( cyrillic2hex( l[i] ) );
-            else:
-                f.write( "0x0020U" );
-            if i != ( maxUnitsLen - 1 ):
-                f.write( ", " );
-            i = i + 1;
-        f.write( " },\n" );
-        if ( row.bitMapSize > 0 ):
-            f.write( "   .type       = " + setClass( 'B' ) + ",\n" );
-        else:
-            f.write( "   .type       = " + setClass( row.type ) + ",\n" );
-        f.write( "   .len        = " + str( row.len )   + "U,\n" );
-        f.write( "   .bitMapSize = " + str( row.bitMapSize ) + "U,\n" );
-        if ( row.bitMapSize > 0 ):
-            f.write( "   .bitMap     = " + row.name + "BitMap\n" );
-        else:
-            f.write( "   .bitMap     = NULL,\n" );
-        if ( row.bitMapSize > meta.maxBitMapSize ):
-            meta.maxBitMapSize = row.bitMapSize;
-        meta.configStorageSize += ( row.len * 2 ) + 1;
-        f.write( '};\n' );
-        if ( row.rw == "r" ):
-            f.write( "const " );
-        f.write( "eConfigReg " + row.name + " =\n" );
-        postArray       += "&" + row.name + ", ";
-        dictionaryArray += '"' + row.str + '", ';
-        f.write( '{\n');
-        f.write( "   .atrib = &" + row.name + "Atrib,\n" );
-        f.write( "   .value = " + row.name + "Value,\n" );
-        f.write( "};\n" );
-        f.write( "/*----------------------------------------------------------------*/\n" );
+        recordToStruct( row, f, meta );
     f.write( "\n" );
-    postArray       = postArray[:-1];
-    postArray       = postArray[:-1];
-    dictionaryArray = dictionaryArray[:-1];
-    dictionaryArray = dictionaryArray[:-1];
-    f.write( postArray + "};\n" );
-    f.write( dictionaryArray + "};\n" );
+    meta.postArray       = meta.postArray[:-1];
+    meta.postArray       = meta.postArray[:-1];
+    meta.dictionaryArray = meta.dictionaryArray[:-1];
+    meta.dictionaryArray = meta.dictionaryArray[:-1];
+    f.write( meta.postArray + "};\n" );
+    f.write( meta.dictionaryArray + "};\n" );
     f.write( "const fix16_t     scaleMulArray[CONFIG_SCALE_NUM] = { " );
     for i in range( meta.maxScale - meta.minScale + 1 ):
         f.write( str( fix16_from_float ( pow( 10, ( meta.minScale + i ) ) ) ) );
@@ -389,23 +396,34 @@ def makeC_config ( map, fileName, structName ):
     return meta;
 #************************************** C **************************************
 def makeC_output (  map, fileName, structName ):
+    print( ">>Make C output...");
     if ( fileName == "no"):
         fileName = 'output.c'
     meta = metaDataC();
     f    = codecs.open( fileName, "w+", "utf-8" );
     f.write( "/*\n" );
     f.write( " */\n" );
-    f.write( "#include   \"config.h\"\n" );
-    f.write( "#include   \"output.h\"\n" );
+    f.write( "#include   \"outputData.h\"\n" );
     f.write( "\n" );
-    postArray       = "eConfigReg* const " + structName + "Reg[OUTPUT_REGISTER_NUMBER]  = { ";
-    dictionaryArray = "const char*       " + structName + "Dictionary[OUTPUT_REGISTER_NUMBER] = { ";
-
+    meta.postArray       = "eConfigReg* const " + structName + "Reg[OUTPUT_DATA_REGISTER_NUMBER]  = { ";
+    meta.dictionaryArray = "const char*       " + structName + "Dictionary[OUTPUT_DATA_REGISTER_NUMBER] = { ";
+    for row in map:
+        recordToStruct( row, f, meta );
+    f.write( "\n" );
+    meta.postArray       = meta.postArray[:-1];
+    meta.postArray       = meta.postArray[:-1];
+    meta.dictionaryArray = meta.dictionaryArray[:-1];
+    meta.dictionaryArray = meta.dictionaryArray[:-1];
+    f.write( meta.postArray + "};\n" );
+    f.write( meta.dictionaryArray + "};\n" );
+    f.close();
+    print( "Done! ");
+    return meta;
 #************************************** H **************************************
 def makeH_config ( map, meta, fileName ):
     print( ">>Make H..." );
     if ( fileName == "no"):
-        fileName = 'output.h'
+        fileName = 'config.h'
     maxLen = 0;
     for row in map:
         if row.len > maxLen:
@@ -424,7 +442,7 @@ def makeH_config ( map, meta, fileName ):
     f.write( "/*------------------------ Define --------------------------------------*/\n" );
     f.write( "#define   MAX_UNITS_LENGTH             " + str( maxUnitsLen ) + "U\n" );
     f.write( "#define   MAX_BIT_MAP_LENGTH           " + str( meta.maxBitMapSize ) + "U\n" );
-    f.write( "#define   SETTING_REGISTER_NUMBER      " + str( maxRegNumber + 1 ) + "U\n" );
+    f.write( "#define   SETTING_REGISTER_NUMBER      " + str( regNumber ) + "U\n" );
     f.write( "#define   FILDS_TO_WRITE_NUMBER        3U\n" );
     f.write( "#define   BROADCAST_ADR                0xFFFFU\n" );
     f.write( "#define   MAX_VALUE_LENGTH             " + str( maxLen ) + "U\n" );
@@ -520,6 +538,69 @@ def makeH_config ( map, meta, fileName ):
     print( "Done!" );
     return;
 #******************************************************************************
+def makeH_output ( map, meta, fileName ):
+    print( ">>Make H..." );
+    if ( fileName == "no"):
+        fileName = 'outputData.h'
+    maxLen = 0;
+    for row in map:
+        if row.len > maxLen:
+            maxLen = row.len
+    maxOutputSize = maxUnitsLen * 2 + 1 + maxLen * 2 + meta.maxBitMapSize * 3;
+    f = codecs.open( fileName,"w+","utf-8" );
+    f.write( "/*\n" );
+    f.write( " */\n" );
+    f.write( "/*----------------------------------------------------------------------*/\n" );
+    f.write( "#ifndef INC_OUTPUT_DATA_H_\n" );
+    f.write( "#define INC_OUTPUT_DATA_H_\n" );
+    f.write( "/*----------------------- Includes -------------------------------------*/\n" );
+    f.write( "#include \"stm32f2xx_hal.h\"\n" );
+    f.write( "#include \"config.h\"\n" );
+    f.write( "#include \"fix16.h\"\n" );
+    f.write( "/*------------------------ Define --------------------------------------*/\n" );
+    f.write( "#define   MAX_UNITS_OUTPUT_LENGTH             " + str( maxUnitsLen ) + "U\n" );
+    f.write( "#define   MAX_BIT_MAP_OUTPUT_LENGTH           " + str( meta.maxBitMapSize ) + "U\n" );
+    f.write( "#define   OUTPUT_DATA_REGISTER_NUMBER         " + str( regNumber ) + "U\n" );
+    f.write( "#define   FILDS_TO_WRITE_OUTPUT_NUMBER        3U\n" );
+    f.write( "#define   MAX_VALUE_OUTPUT_LENGTH             " + str( maxLen ) + "U\n" );
+    f.write( "#define   OUTPUT_DATA_MAX_SIZE                " + str( maxOutputSize ) + "U     // bytes\n" );
+    f.write( "#define   OUTPUT_DATA_TOTAL_SIZE              " + str( meta.configStorageSize ) + "U   // bytes\n" );
+    f.write( "#define   MIN_OUTPUT_DATA_SCALE               ( " + str( meta.minScale ) + " )\n" );
+    f.write( "#define   MAX_OUTPUT_DATA_SCALE               ( " + str( meta.maxScale ) + " )\n");
+    f.write( "#define   OUTPUT_DATA_SCALE_NUM               ( " + str( meta.maxScale - meta.minScale + 1 ) + " )\n");
+    f.write( "\n" );
+    f.write( "/*----------------------- Structures -----------------------------------*/\n" );
+    f.write( "/*---------------------- Register addresses ----------------------------*/\n" );
+    for row in map:
+        strAdr = "";
+        index  = 0;
+        f.write( "#define   " );
+        for i in range( len( row.name ) ):
+            if ( row.name[i].isupper() == True ):
+                strAdr += row.name[index : i].upper() + "_";
+                index   = i;
+        strAdr += row.name[index : ].upper() + "_ADR";
+        f.write( strAdr );
+        for i in range( len( strAdr ), 55, 1 ):
+            f.write( " " );
+        f.write( str( row.adr ) + 'U\n' );
+    f.write( "/*---------------------- Bitmap addresses ------------------------------*/\n" );
+    f.write( meta.bitAdresses );
+    f.write( "/*------------------------- Extern -------------------------------------*/\n" );
+    for row in map:
+        f.write( "extern " );
+        if ( row.rw == "r" ):
+            f.write( "const " );
+        f.write( "eConfigReg " + str( row.name ) + ";\n" );
+    f.write( "extern eConfigReg* const outputDataReg[OUTPUT_DATA_REGISTER_NUMBER];\n" );
+    f.write( "extern const char*       outputDataDictionary[OUTPUT_DATA_REGISTER_NUMBER];\n" );
+    f.write( "extern const fix16_t     scaleMulArray[OUTPUT_DATA_SCALE_NUM];\n" );
+    f.write( "/*----------------------------------------------------------------------*/\n" );
+    f.write( "#endif /* INC_OUTPUT_DATA_H_ */\n" );
+    f.close();
+    print( "Done!" );
+    return;
+#******************************************************************************
 def getArg ( args, key ):
     res = "no";
     for i in range( 0, len( args ) ):
@@ -529,12 +610,13 @@ def getArg ( args, key ):
 #******************************************************************************
 if __name__ == "__main__":
     map = [];
-    [map, maxUnitsLen, maxRegNumber] = readCSV( 'config.csv' );
+    [map, maxUnitsLen, maxRegNumber, regNumber] = readCSV( 'config.csv' );
     makeJSON( map, 'C:\PROJECTS\ENERGAN\web-face\js\config.js', 'dataReg' );
     metaData  = makeC_config( map, 'C:\PROJECTS\ENERGAN\energan_enb\data\Src\config.c', 'config' );
     makeH_config ( map, metaData, 'C:\PROJECTS\ENERGAN\energan_enb\data\Inc\config.h' );
     print( ">>Total size: " + str( metaData.totalSize ) );
 
-    [map, maxUnitsLen, maxRegNumber]  = readCSV( 'outputData.csv' );
+    [map, maxUnitsLen, maxRegNumber, regNumber]  = readCSV( 'outputData.csv' );
     makeJSON( map, 'C:\PROJECTS\ENERGAN\web-face\js\output.js', 'outputReg' );
-    #metaData  = makeC( map, 'C:\PROJECTS\ENERGAN\energan_enb\data\Src\outputData.c', 'outputData' );
+    metaData  = makeC_output( map, 'C:\PROJECTS\ENERGAN\energan_enb\data\Src\outputData.c', 'outputData' );
+    makeH_output ( map, metaData, 'C:\PROJECTS\ENERGAN\energan_enb\data\Inc\outputData.h' );
